b1e15d8ef9fc9d4637689e7b2c8a2c62f72c4474
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-30 13:47:05 +0430

    create teamchannelPreview

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useChatContext, Avatar } from "stream-chat-react";
 
-const TeamChannelPreview = () => {
+const TeamChannelPreview = ({ channel, type }) => {
+    const { channel: activeChannel, client } = useChatContext();
+
+    const ChannelPreview = () => <p className="channel-preview__item"># {channel?.data?.name || channel?.data?.id}</p>;
+
+    const DirectPreview = () => (
+        const members = Object.values(channel.state.members).filter(({user}) => user.id !== client.userID)
+    )
+
     return <div>TeamChannelPreview</div>;
 };
 
